f398ed2d48e9d8f557a4fa568e50f0f3339f75a6
carlosmg2005 <carlosmg2005@msn.com>
2023-03-26 21:11:15 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio CSharp Cachorro-Quente/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_CSharp_Cachorro_Quente
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            string[] entrada = Console.ReadLine().Split();
+            int participantes = int.Parse(entrada[0]);
+            int cachorrosQuentes = int.Parse(entrada[1]);
+
+            //TODO: Calcular a média de cachorros-quentes consumidos pelos participantes.
+            double media = (double)participantes / cachorrosQuentes;
+
+            Console.WriteLine("{0}", media.ToString("N2"));
+
+            Console.ReadKey();
+            }
+        }
+}
